722318707311871a769c29afb960a17d154f4e61
Smilindeth <damaster3001@yahoo.com>
2015-08-08 23:08:43 -0600

    Refixed score

--- Crystal Ray/Assets/Scripts/score.cs
+++ Crystal Ray/Assets/Scripts/score.cs
@@ -27,12 +27,17 @@ public class score : MonoBehaviour {
 			globalData.tag = "Global";
 			globalData.name = "Global Data";
 		}
+
+	}
+
+	void Update()
+	{
 		//Grabs the Children text under the GameObject
 		texts = canvas.gameObject.GetComponentsInChildren <Text> ();
-
+		
 		//Sets text of Current Score
 		texts[0].text = "Current: " + globalData.GetComponent<gameVariables>().GetSetLevelsCompleted.ToString();
-
+		
 		//Sets text of High Score
 		texts[1].text = "High Score: " + PlayerPrefs.GetInt("Score").ToString();
 	}
